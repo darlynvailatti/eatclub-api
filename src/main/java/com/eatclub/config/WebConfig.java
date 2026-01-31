c9bbae505916fe8579716b523e0abe8f0b7f25c6
Darlyn Vailatti <darlyn.vailatti@shippo.com>
2026-02-01 09:18:20 +1030

    Chore: Adds Web App Configuration for Spring Boot - read global API base path

---
+++ src/main/java/com/eatclub/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.eatclub.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${api.base-path}")
+    private String apiBasePath;
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix(apiBasePath, c -> c.isAnnotationPresent(org.springframework.web.bind.annotation.RestController.class));
+    }
+}
